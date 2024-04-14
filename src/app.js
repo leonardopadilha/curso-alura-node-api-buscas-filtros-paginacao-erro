b075c6f3595d1ddf86f224cbfed40d22cabba6e2
leonardopadilha <leonardopadilha28@gmail.com>
2024-04-14 10:42:52 -0300

    Middleware - Manipulador de erro 404

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import routes from "./routes/index.js";
+import manipulador404 from "./middlewares/Manipulador404.js";
 import manipuladorDeErros from "./middlewares/manipuladorDeErros.js";
 
 db.on("error", function(err){
@@ -15,6 +16,7 @@ const app = express();
 app.use(express.json());
 routes(app);
 
+app.use(manipulador404);
 app.use(manipuladorDeErros);
 
 export default app;
