5821901315bda2fc081ef09c8a7e0e9c174b4c04
leonardopadilha <leonardopadilha28@gmail.com>
2024-05-05 12:32:44 -0300

    Ordenando os resultados

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -6,14 +6,18 @@ class LivroController {
 
   static listarLivros = async (req, res, next) => {
     try {
-      let { limite = 5, pagina = 1 } = req.query;
+      let { limite = 5, pagina = 1, ordenacao = "_id:1" } = req.query;
+
+      let [campoOrdenacao, ordem] = ordenacao.split(":");
 
       limite = parseInt(limite);
       pagina = parseInt(pagina);
+      ordem = parseFloat(ordem);
 
       if (limite > 0 && pagina > 0) {
         const livrosResultado = await livros.find()
-          .skipt((pagina - 1) * limite)
+          .sort({ [campoOrdenacao]:ordem })
+          .skip((pagina - 1) * limite)
           .limit(limite)
           .populate("autor")
           .exec();
